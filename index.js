93ae38d2ce1a3c2fef9b97082432d46b3a2da259
zidanegimiga <zidanegimiga@gmail.com>
2023-03-30 13:56:06 +0300

    add pdf route

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
+const fs = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/pdf/ditoro', (req, res) => {
+    const filePath = './pdf/ditoro.pdf';
+    const fileName = 'ditoro.pdf';
+    const file = fs.createReadStream(filePath);
+    const stat = fs.statSync(filePath);
+  
+    res.setHeader('Content-Length', stat.size);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+  
+    file.pipe(res);
+  });
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("App is listening at ")
+})
