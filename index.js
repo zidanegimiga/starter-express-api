42b1b1ed69bb79da5fb6f7c0689af3873c2739b4
zidanegimiga <zidanegimiga@gmail.com>
2023-03-30 14:20:38 +0300

    fixes

--- index.js
+++ index.js
@@ -8,18 +8,9 @@ app.get('/', (req, res) => {
     res.send('Yo!')
 })
 
-app.get('/pdf/ditoro', (req, res) => {
-    const filePath = './pdf/ditoro.pdf';
-    const fileName = 'ditoro.pdf';
-    const file = fs.createReadStream(filePath);
-    const stat = fs.statSync(filePath);
-  
-    res.setHeader('Content-Length', stat.size);
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
-  
-    file.pipe(res);
-  });
+app.get("/pdf/ditoro", (req, res) => {
+    res.download("./pdf/ditoro.pdf");
+});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("App is listening at ")
